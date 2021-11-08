e5acec1384bda062c9a212b7a51e7e59958e948d
Julien <julien.aberton@edu.devinci.fr>
2021-11-08 18:46:58 +0100

    inputAdjustment

--- maths.cpp
+++ maths.cpp
@@ -6,15 +6,15 @@ void PlayerMovement(sf::CircleShape& circle, float deltaTime)
 {
 	float speed = 300.f; // 300 pixels par seconde
 
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::Z))
 		circle.move(sf::Vector2f(0.f, -speed * deltaTime));
 
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) || sf::Keyboard::isKeyPressed(sf::Keyboard::S))
 		circle.move(sf::Vector2f(0.f, speed * deltaTime));
 
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left) || sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
 		circle.move(sf::Vector2f(-speed * deltaTime, 0.f));
 
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D))
 		circle.move(sf::Vector2f(speed * deltaTime, 0.f));
 }
