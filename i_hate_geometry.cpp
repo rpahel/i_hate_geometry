11d1442855f448900afa5edbc8004a42cfeebc0e
rpahel <92730433+rpahel@users.noreply.github.com>
2021-11-08 18:35:01 +0100

    Movement

--- i_hate_geometry.cpp
+++ i_hate_geometry.cpp
@@ -1,26 +1,63 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
+void RectMovement(sf::CircleShape& circle, float deltaTime);
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1200, 900), "ChronoSpacer");
-    // Initialise everything below
-    sf::CircleShape circle;
-    circle.setRadius(100);
-    circle.setFillColor(sf::Color::Red);
+	sf::RenderWindow window(sf::VideoMode(1200, 900), "Ma première fenêtre");
+	window.setVerticalSyncEnabled(true);
 
-    // Game loop
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            // Process any input event here
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-        }
-        window.clear();
-        // Whatever I want to draw goes here
-        window.draw(circle);
-        window.display();
-    }
+	sf::CircleShape circle(50);
+	circle.setPosition(sf::Vector2f(400.f, 300.f));
+
+	sf::Clock clock;
+
+	while (window.isOpen())
+	{
+		// Inputs
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+
+			default:
+				break;
+			}
+		}
+
+		// Logique
+		sf::Time elapsedTime = clock.restart(); //< Calcul du temps écoulé depuis la dernière boucle
+
+		RectMovement( circle, elapsedTime.asSeconds());
+
+		// Rendu
+		window.clear();
+
+		window.draw(circle);
+
+		window.display();
+	}
+}
+
+// Gestion des déplacement d'un rectangle
+void RectMovement(sf::CircleShape& circle, float deltaTime)
+{
+	float speed = 300.f; // 300 pixels par seconde
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+		circle.move(sf::Vector2f(0.f, -speed * deltaTime));
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+		circle.move(sf::Vector2f(0.f, speed * deltaTime));
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+		circle.move(sf::Vector2f(-speed * deltaTime, 0.f));
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+		circle.move(sf::Vector2f(speed * deltaTime, 0.f));
 }
